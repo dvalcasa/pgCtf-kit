6b346d10356b228960042ff194c7b66a386d9710
Didier Valcasara <dvalcasa@icloud.com>
2026-08-21 23:55:37 +0200

    Fix public init

--- Sources/pgCtfKit/DTOs/Location/CreateLocationRequest.swift
+++ Sources/pgCtfKit/DTOs/Location/CreateLocationRequest.swift
@@ -7,10 +7,22 @@
 
 import Foundation
 
-public struct CreateLocationRequest {
+public struct CreateLocationRequest: Encodable, Sendable {
     let latitude: Double
     let longitude: Double
     let altitude: Double
     let timestamp: Date
     let playerId: UUID
+    
+    public init(latitude: Double,
+         longitude: Double,
+         altitude: Double,
+         timestamp: Date,
+         playerId: UUID) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+        self.timestamp = timestamp
+        self.playerId = playerId
+    }
 }
